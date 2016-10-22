b4dec3d62aa3dd428d073b2f060d13787a463c02
Hjörtur Waltersson <hjorturw@gmail.com>
2016-10-22 13:51:23 +0000

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,56 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+/*
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div clasName="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by Hjörtur Waltersson</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});*/
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://www.hjortur.com" target="_blank">Hjörtur</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
